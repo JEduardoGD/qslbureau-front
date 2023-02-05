9de7cdcac06d621b21ec5e21c53c531e393a1b0e
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2023-02-05 09:44:27 -0600

    generacion componente

---
+++ src/app/qsl-captura/qsl-captura.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qsl-captura',
+  templateUrl: './qsl-captura.component.html',
+  styleUrls: ['./qsl-captura.component.css']
+})
+export class QslCapturaComponent {
+
+}
