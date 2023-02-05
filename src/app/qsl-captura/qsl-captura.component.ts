b31ccc3aff6aceb517edc6c4914976a851cf220c
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2023-02-05 10:26:44 -0600

    adding form captura qsl

--- src/app/qsl-captura/qsl-captura.component.ts
+++ src/app/qsl-captura/qsl-captura.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-qsl-captura',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./qsl-captura.component.css']
 })
 export class QslCapturaComponent {
+  checkoutForm;
 
+  constructor(fb: FormBuilder)
+  {
+      this.checkoutForm = fb.group({
+        qslto: ["", Validators.required]
+      });
+  }
+
+
+  onSubmit(customerData:any) {
+    // Process checkout data here
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted' + customerData);
+  }
 }
