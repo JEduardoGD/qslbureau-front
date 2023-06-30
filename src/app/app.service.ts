9b5d524af3ca89988b88c64440e66049edcd1856
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2023-06-29 23:46:06 -0600

    se convierte captura qsl a promesa

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -16,42 +16,52 @@ export class AppService {
   qslCardUrl = '/qslcard';
   errorMessage: string | undefined;
 
-  captureQsl(qslcard: Qslcard) {
+  captureQslProm(qslcard: Qslcard){
     let auth_token = localStorage.getItem('auth_token');
-
-    let httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        Authorization: `Bearer ${auth_token}`
-      })
-    };
-    this.http.put<Qslcard>(environment.apiUrl + this.qslCardUrl, qslcard, httpOptions)
-    .pipe(catchError((error: any, caught: Observable<any>): Observable<any> => {
-      this.errorMessage = error.message;
-      console.error('There was an error!', error);
-      // after handling error, return a new observable 
-      // that doesn't emit any values and completes
-      if(error.status == HttpStatusCode.Unauthorized){
-        Swal.fire({
-          icon: 'error',
-          title: `Las credenciales han expirado.`
-        }).then(() =>{
-          this.router.navigate(['/logout']);
-        });
-      }
-      return of();
-  }))
-  .subscribe(data => {
-    console.log(data);
-    let str = data.jsonPayload;
-    let obj :Qslcard = JSON.parse(str);
-    Swal.fire({
-      icon: 'success',
-      title: `Guardado, slot ${obj.slotNumber}!`,
-      html: `Qsls en el slot: <b>${obj.qslsInSlot}</b>`,
-      timer: 3000
+    return new Promise((resolve, reject) => {
+      let httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          Authorization: `Bearer ${auth_token}`
+        })
+      };
+      this.http.put<Qslcard>(environment.apiUrl + this.qslCardUrl, qslcard, httpOptions)
+      .pipe(catchError((error: any, caught: Observable<any>): Observable<any> => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', error);
+        // after handling error, return a new observable 
+        // that doesn't emit any values and completes
+        if(error.status == HttpStatusCode.Unauthorized){
+          Swal.fire({
+            icon: 'error',
+            title: `Las credenciales han expirado.`
+          }).then(() =>{
+            this.router.navigate(['/logout']);
+          });
+        }
+        return of();
+      }))
+      .subscribe(data => {
+        if(!data.error){
+          console.log(data);
+          let str = data.jsonPayload;
+          let obj :Qslcard = JSON.parse(str);
+          Swal.fire({
+            icon: 'success',
+            title: `Guardado, slot ${obj.slotNumber}!`,
+            html: `Qsls en el slot: <b>${obj.qslsInSlot}</b>`,
+            timer: 3000
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: `Ha ocurrido un error`,
+            text: `${data.errorMessage}`,
+            timer: 3000
+          });
+        }
+      });
     });
-  });
   }
 
   qslsByLocalId(localId: string | null): Observable<RowObject[]>{
@@ -81,3 +91,4 @@ export class AppService {
   }))
   }
 }
+
