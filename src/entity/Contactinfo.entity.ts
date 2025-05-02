3e0551e810bf04112c63205236cb9f165ee187aa
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2025-05-01 22:35:28 -0600

    email sending

---
+++ src/entity/Contactinfo.entity.ts
@@ -0,0 +1,12 @@
+export interface Contactinfo {
+    idContact: number;
+    name: string;
+    surename: string;
+    callsign: string;
+    address: string;
+    email: string;
+    whatsapp: string;
+    wantemail: boolean;
+    start: Date | null;
+    end: Date | null;
+}
---
+++ src/entity/Qslinfo.entity.ts
@@ -0,0 +1,7 @@
+export interface Qslinfo {
+    localId: number;
+    slotNumber: number;
+    toCallsign: string;
+    via: string | null;
+    c: number;
+}
